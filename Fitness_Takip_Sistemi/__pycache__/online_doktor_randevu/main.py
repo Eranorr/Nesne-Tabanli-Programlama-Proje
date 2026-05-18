f49f0cf2087e11f540ef4901bfe9141b2121adc6
İsmail Can Çakır <ismailcancakir1903@gmail.com>
2026-05-18 17:13:34 +0300

    Delete Fitness_Takip_Sistemi/__pycache__ directory

--- Fitness_Takip_Sistemi/__pycache__/online_doktor_randevu/main.py
+++
@@ -1,9 +0,0 @@
-﻿if __name__ == "__main__":
-    # Allow running from any working directory.
-    import sys
-    from pathlib import Path
-
-    sys.path.insert(0, str(Path(__file__).resolve().parent))
-    from app import run
-
-    run()
